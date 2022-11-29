aa2471d7e25496380982cf0b3cce29690f15b004
Karan Rajagopalan <karan.rajagopalan@cognite.com>
2022-11-29 15:22:36 +0100

    updated the transformation sample

---
+++ config/transformations/assets_transform_sample/my_transformation.sql
@@ -0,0 +1,9 @@
+SELECT external_id                                             AS externalId,IF(parent_external_id = ''
+           OR parent_external_id IS NULL, '', parent_external_id) AS
+       parentExternalId,
+       name                                                   AS name,
+	   to_metadata(metadata) 								  AS metadata, 
+       description                                            AS description,
+       'from RAW'                                             AS source,
+       Dataset_id('src:001:sap:ds')                           AS dataSetId
+FROM   `src:001:sap:db`.assets
